41c0eda0a0a56de9f2dd2a1daf7b3f7d3d759860
Programmer.Lotus <77867932+Rishabg24@users.noreply.github.com>
2023-01-06 20:13:59 -0800

    Add files via upload

---
+++ password.py
@@ -0,0 +1,49 @@
+import string
+import re
+import random
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = '0123456789'
+special = string.punctuation 
+
+print('Welcome!')
+print('Would you like to generate or strengthen your passsword')
+choice = input()
+
+if choice == 'Generate':
+  
+  print('how many digits would you like it to be')
+  length = int(input())
+  
+  string = lower + upper + num + special
+  password = "".join(random.sample(string, length))
+  
+  print('Your new password is : ', password)
+
+  exit()
+
+if choice == 'Strengthen':
+  
+  print('enter your password')
+  password = input()
+  
+  if (len(password)) <= 20:
+    print('Consider creating 20 character passwords for optimal strength. ')
+    
+  elif not re.search('[a-z]', password):
+    print('You should consider adding some lower case letters in you password')
+    
+  elif not re.search('[A-Z]', password):
+    print('You should consider adding some Upper case letters in your password')
+    
+  elif not re.search('[0-9]', password):
+    print('You should consider adding some numbers to your password')
+
+  elif not any(c in special for c in password):
+    print('You should consider adding some special characters to your password')
+
+else:
+  print('you have a strong password!')
+
+
